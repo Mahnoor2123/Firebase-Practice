ee56ad00512ed10cdc42aadb2f58555140b246e9
Mahnoor2123 <mahikhan2999@gmail.com>
2023-03-02 11:35:56 +0500

    single blog edit done

--- api/Blog-Detail.js
+++ api/Blog-Detail.js
@@ -1,58 +1,55 @@
 let urlParam = new URLSearchParams(window.location.search);
-let myParam= urlParam.get("id");
-console.log(myParam); 
+let myParam = urlParam.get("id");
+console.log(myParam);
 
-db.collection("Blogs").doc(myParam).get().then((blogData)=>{
-console.log(blogData.data());
+db.collection("Blogs").doc(myParam).get().then((blogData) => {
+    console.log(blogData.data());
 
-$("#blogs").append(`
-<div class="blog">
-<h3 id="blogTitle">${blogData.data().blogTitle}</h3>
-<p id="blogDescription">${blogData.data().blogDescription}</p>
-</div> 
-
-
-
+    $("#blogs").append(`
+        <div class="blog">
+        <h3>${blogData.data().blogTitle}</h3>
+        <p >${blogData.data().blogDescription}</p>
+        </div> 
 `)
-$("#blogTitle").val(blogData.data().blogTitle)
-$("#blogDescription").val(blogData.data().blogDescription)
+    $('#blogTitle').val(blogData.data().blogTitle);
+    $('#blogDescription').val(blogData.data().blogDescription)
 })
-.catch((err) => {
-    window.alert(err.message)
-});
-
-
-
-
-
-function Edit(){
-let updatedblogTitle = $("#blogTitle").val();
-let updatedblogDescription = $("#blogDescription").val();
-
-if (updatedblogTitle == ""|| updatedblogDescription == "") {
-    window.alert("Fill all fields")
-    
-} else 
-    db.collection("Blogs").doc(myParam).update(
-        {
-            blogTitle:updatedblogTitle,
-            blogDescription:updatedblogDescription
-        }
-
-    ).then(() => {
-        window.alert("Updated sucessfully");
-        window.location.reload()
-    }).catch((err) => {
-        
+    .catch((err) => {
+        window.alert(err.message)
     });
+
+
+
+
+
+function Edit() {
+    let updatedblogTitle = $("#blogTitle").val();
+    let updatedblogDescription = $("#blogDescription").val();
+
+    if (updatedblogTitle == "" || updatedblogDescription == "") {
+        window.alert("Fill all fields")
+
+    } else
+        db.collection("Blogs").doc(myParam).update(
+            {
+                blogTitle: updatedblogTitle,
+                blogDescription: updatedblogDescription
+            }
+
+        ).then(() => {
+            window.alert("Updated sucessfully");
+            window.location.reload()
+        }).catch((err) => {
+
+        });
 }
 
-function Delete(){
-    db.collection('Blogs').doc(myParam).delete().then(()=>{
-    window.alert("Deleted");
-    window.location="../Home.html"
+function Delete() {
+    db.collection('Blogs').doc(myParam).delete().then(() => {
+        window.alert("Deleted");
+        window.location = "../Home.html"
 
     }
-     
+
     )
 }
--- api/addblog.js
+++ api/addblog.js
@@ -3,12 +3,14 @@ let myId = localStorage.getItem('userId');
 console.log(myId);
 
 function addBlog() {
+
     let blogTitle = document.getElementById("blogTitle").value;
     let blogDescription = document.getElementById("blogDescription").value;
     if (blogTitle == "" || blogDescription == "") {
         window.alert("Enter all Fields")
 
     } else {
+        $('#submitBtn').html('Please Wait....')
         let blogRef = db.collection("Blogs").doc();
         let data = {
             blogTitle: blogTitle,
@@ -20,11 +22,11 @@ function addBlog() {
         blogRef.set(data).then(() => {
             window.alert('Blog Added Successfully');
             window.location.reload();
-            window.location.href="../Home.html"
-            localStorage.setItem('blogId' );
-            
+            window.location.href = "../Home.html"
+            localStorage.setItem('blogId');
+
         }).catch((err) => {
-            
+            window.alert(err.message);
         });
 
     }
